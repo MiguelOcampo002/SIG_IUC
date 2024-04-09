a629d9fea1be911e04dbddcc06ddca782a70d120
MiguelOcampo002 <163178570+MiguelOcampo002@users.noreply.github.com>
2024-04-09 13:41:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,101 +2,90 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import geopandas as gpd
+from PIL import Image  # Importa la clase Image desde el módulo PIL
 import json
 
+# Abre el archivo GeoJSON
 with open('Accidentalidad Vial Municipio de Medellín 2016.geojson', "r") as read_file:
     data = json.load(read_file)
 
+# Título de la aplicación
 st.title("Accidentalidad Municipio de Medellín 2016")
 
-st.write('Se entiende por accidente de tránsito  evento, generalmente involuntario, generado al menos por un un vehículo en movimiento, que causa daños a '
-         'personas y bienes involucrados en él, e igualmente afecta la normal circulación de los vehículos que se movilizan por la vía o vías comprendidas en el' 
-         'lugar o dentro de la zona de influencia del hecho0 (Ley 769 de 2002 - Código Nacional de Tránsito)'
-         )
+# Introducción
+st.write('Se entiende por accidente de tránsito evento, generalmente involuntario, generado al menos por un vehículo en movimiento, que causa daños a '
+         'personas y bienes involucrados en él, e igualmente afecta la normal circulación de los vehículos que se movilizan por la vía o vías comprendidas en el ' 
+         'lugar o dentro de la zona de influencia del hecho0 (Ley 769 de 2002 - Código Nacional de Tránsito)')
 st.subheader('Sistema de consulta de Accidentalidad municipio de Medellín')
-image= Image.open('AccV.JPEG')
+
+# Carga de imagen
+image = Image.open('AccV.JPEG')
 st.image(image, caption="Accidentes viales")
 
+# Listas para almacenar los datos
 La = []
-Lo= []
-day=[]
-hour=[]
-neig=[]
-dir=[]    
+Lo = []
+day = []
+hour = []
+neig = []
+dir = []
 
-# Decodificar el archivo en formato JSON (Java Script Object Notation)
+# Decodificación del archivo en formato JSON
 for feature in data['features']:
     coordinates = feature['geometry']['coordinates']
-    dia=feature['properties']['dia']
-    Hora=feature['properties']['hora']
-    barrio=feature['properties']['barrio']
-    direccion=feature['properties']['direccion']
+    dia = feature['properties']['dia']
+    Hora = feature['properties']['hora']
+    barrio = feature['properties']['barrio']
+    direccion = feature['properties']['direccion']
     La.append(coordinates[1])
-    Lo.append(coordinates[0])  
+    Lo.append(coordinates[0])
     day.append(dia)
     hour.append(Hora)
     neig.append(barrio)
     dir.append(direccion)
-    
-nm= st.slider('Selecciona el número de registros de accidentes quieres visualizar', 5, 30000)
-#Construir la tabla de datos (dataframe)
-dfLa = pd.DataFrame({'lat':La[0 : nm]})
-dfLo = pd.DataFrame({'lon':Lo[0 : nm]})
-dfdia= pd.DataFrame({'día' :day[0:nm]})
-dfhor= pd.DataFrame({'Hora' :hour[0:nm]})
-dfbarr=pd.DataFrame({'Barrio':neig[0:nm]})
-dfdir=pd.DataFrame({'Dirección':dir[0:nm]})
-df_g=pd.concat([dfLa, dfLo, dfdia, dfhor,dfdir,dfbarr], axis=1)
 
-# Mostrar la tabla de datos (dataframe)
+# Slider para seleccionar el número de registros de accidentes a visualizar
+nm = st.slider('Selecciona el número de registros de accidentes que deseas visualizar', 5, 30000)
+
+# Construir el DataFrame con los datos obtenidos
+dfLa = pd.DataFrame({'lat': La[0:nm]})
+dfLo = pd.DataFrame({'lon': Lo[0:nm]})
+dfdia = pd.DataFrame({'día': day[0:nm]})
+dfhor = pd.DataFrame({'Hora': hour[0:nm]})
+dfbarr = pd.DataFrame({'Barrio': neig[0:nm]})
+dfdir = pd.DataFrame({'Dirección': dir[0:nm]})
+df_g = pd.concat([dfLa, dfLo, dfdia, dfhor, dfdir, dfbarr], axis=1)
+
+# Mostrar la tabla de datos
 st.dataframe(df_g)
-#Dibujar el mapa utilizando las columnas 'lat', 'lon'.
+
+# Dibujar el mapa utilizando las columnas 'lat' y 'lon'
 st.map(df_g)
 
-# Realizar un filtrado de los datos
-st.subheader('Filtrado')
+# Realizar un filtrado de los datos por día y hora
+st.subheader('Filtrado por Día y Hora')
 option_hour_min = st.selectbox('Selecciona filtro por Hora',
-                               ('08:00:00', '09:00:00', '10:00:00','11:00:00','12:00:00','13:00:00','14:00:00'),key='1')
-#option_hour_max = st.selectbox('Selecciona filtro por Hora',
-#                               ('08:00:00', '09:00:00', '10:00:00','11:00:00','12:00:00','13:00:00','14:00:00'),key='2')
-option_day = st.selectbox('Selecciona filtro por día',('LUNES', 'MARTES', 'MIÉRCOLES','JUEVES','VIERNES','SÁBADO','DOMINGO'))
-#df_filtrado = df_g.query('día == "MIÉRCOLES" and Hora >= "08:00:00" and Hora <= "10:00:00"')
-df_filtrado = df_g.query('día == @option_day and Hora >=  @option_hour_min ')
+                               ('08:00:00', '09:00:00', '10:00:00', '11:00:00', '12:00:00', '13:00:00', '14:00:00'),
+                               key='1')
+option_day = st.selectbox('Selecciona filtro por día', ('LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SÁBADO', 'DOMINGO'))
+
+df_filtrado = df_g.query('día == @option_day and Hora >= @option_hour_min')
 st.dataframe(df_filtrado)
 
 try:
-   st.metric("Cantidad de Incidentes dentro del filtro", df_filtrado.shape[0])
+    st.metric("Cantidad de Incidentes dentro del filtro", df_filtrado.shape[0])
 except:
     pass
 
-
 st.map(df_filtrado)
-#st.write(df)
 
-option_ng = st.selectbox('Selecciona filtro por barrio',('La Aguacatala', 'Aranjuez', 'Manrique','Robledo'))
-#df_filtrado = df_g.query('día == "MIÉRCOLES" and Hora >= "08:00:00" and Hora <= "10:00:00"')
-df_filtrado = dfbarr.query('día == @option_ng and Barrio >=  @option_hour_min ')
-st.dataframe(df_filtrado)
-
-try:
-   st.metric("Cantidad de Incidentes dentro del filtro", df_filtrado.shape[0])
-except:
-    pass
-
-
-st.map(df_filtrado)
-#st.write(df)
-# Supongamos que df_g es tu DataFrame con los datos
-
-# Sección de Filtrado por Barrio
+# Realizar un filtrado de los datos por barrio
 st.subheader('Filtrado por Barrio')
 option_barrio = st.selectbox('Selecciona filtro por Barrio',
                              ('La Aguacatala', 'Aranjuez', 'Manrique', 'Robledo'))
 
-# Filtrado de DataFrame por Barrio
-df_filtrado_barrio = dfbarr.query('Barrio == @option_barrio')
+df_filtrado_barrio = df_g.query('Barrio == @option_barrio')
 
-# Visualización de datos
 st.dataframe(df_filtrado_barrio)
 
 try:
@@ -104,5 +93,5 @@ try:
 except:
     pass
 
-# Posiblemente quieras visualizar la ubicación de los incidentes en un mapa
 st.map(df_filtrado_barrio)
+
